aa68d0aa28dfdab3b4bb88957cf9d24b930858f8
Eoin Ocathasaigh <g00417466@atu.ie>
2025-03-27 15:45:08 +0000

    Fixed an error to do with the routing and common module & set things up so that the user is automatically routed to the home page

--- DND-CE/src/app/app.component.ts
+++ DND-CE/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
 
@@ -11,4 +12,12 @@ import { FooterComponent } from "./components/footer/footer.component";
 })
 export class AppComponent {
   title = 'DND-CE';
+
+  //Constructor to be used for dependenct injection
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    //Automatically routing the user to the home page when they boot up the app
+    this.router.navigate(['home']);
+  }
 }
--- DND-CE/src/app/components/navbar/navbar.component.ts
+++ DND-CE/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
