580ea6a874b4f19dd2360aed28db37291f2dc466
Cian Dicker-Hughes <127740176+CianDickerHughes@users.noreply.github.com>
2025-03-27 16:48:20 +0000

    fix and update navigation
    Fix the ngOnInit as it always go to the home page and don't allow user to move or navigate to other pages.
    update the navbar.

--- DND-CE/src/app/app.routes.ts
+++ DND-CE/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CharacterComponent } from './components/character/character.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
+    {path: "character", component: CharacterComponent},
 ];
