138eec0bb5ba153189691e42cd5711a9444d150f
Eoin Ocathasaigh <g00417466@atu.ie>
2025-03-29 09:35:57 +0000

    Added in all possible components as well as set up the basic routing for them

--- DND-CE/src/app/app.routes.ts
+++ DND-CE/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CharacterComponent } from './components/character/character.component';
+import { CombatComponent } from './components/combat/combat.component';
+import { DiceComponent } from './components/dice/dice.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "character", component: CharacterComponent},
+    {path: "combat", component: CombatComponent},
+    {path: "dice", component: DiceComponent},
+    {path: "settings", component: SettingsComponent}
 ];
