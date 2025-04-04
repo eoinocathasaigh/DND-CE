e783b4013380229e46709025eb39195842e3c74a
Eoin Ocathasaigh <g00417466@atu.ie>
2025-04-04 12:51:32 +0100

    Fixed the issues we were experiencing with the navigation inside the app but implementing ionic into the pages

--- DND-CE/src/app/components/home/home.component.ts
+++ DND-CE/src/app/components/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { IonButtons, IonContent, IonHeader, IonImg, IonItem, IonLabel, IonList, IonMenuButton, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [IonLabel, IonItem, IonList, IonImg, IonContent, IonTitle, IonMenuButton, IonButtons, IonToolbar, IonHeader],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
