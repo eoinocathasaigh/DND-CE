200367a562f1861315e26c2b3fbeb6f603c4a59c
Eoin Ocathasaigh <g00417466@atu.ie>
2025-04-13 18:54:45 +0100

    Added a very basic design onto the combat tracker page, installed mongoose for future use in the app. then created a server.js file to handle the retrieval of the data

--- DND-CE/src/app/components/combat/combat.component.ts
+++ DND-CE/src/app/components/combat/combat.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonImg, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-combat',
-  imports: [],
+  imports: [CommonModule, IonHeader, IonButton, IonCardContent, IonImg, IonCardTitle, IonCardHeader, IonCard, IonContent, IonTitle, IonToolbar],
   templateUrl: './combat.component.html',
   styleUrl: './combat.component.css'
 })
