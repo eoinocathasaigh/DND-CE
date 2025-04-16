f1fcc48cc49433e7b8c1492c8c8d570c48d4bb0c
Cian Dicker-Hughes <127740176+CianDickerHughes@users.noreply.github.com>
2025-04-16 21:15:51 +0100

    search works somewhat
    i can't search all the stuff from DND unless they don't exist in the API database

--- DND-CE/src/app/services/open5e.service.ts
+++ DND-CE/src/app/services/open5e.service.ts
@@ -7,27 +7,26 @@ import { Observable } from 'rxjs';
 })
 export class Open5eService {
   private endpoints = {
-    spells: 'https://api.open5e.com/v2/spells/',
+    spells: 'https://api.open5e.com/v1/spells/',
     spelllist: 'https://api.open5e.com/v1/spelllist/',
     monsters: 'https://api.open5e.com/v1/monsters/',
-    documents: 'https://api.open5e.com/v2/documents/',
-    backgrounds: 'https://api.open5e.com/v2/backgrounds/',
+    documents: 'https://api.open5e.com/v1/documents/',
+    backgrounds: 'https://api.open5e.com/v1/backgrounds/',
     planes: 'https://api.open5e.com/v1/planes/',
     sections: 'https://api.open5e.com/v1/sections/',
-    feats: 'https://api.open5e.com/v2/feats/',
-    conditions: 'https://api.open5e.com/v2/conditions/',
-    races: 'https://api.open5e.com/v2/races/',
+    feats: 'https://api.open5e.com/v1/feats/',
+    conditions: 'https://api.open5e.com/v1/conditions/',
+    races: 'https://api.open5e.com/v1/races/',
     classes: 'https://api.open5e.com/v1/classes/',
     magicitems: 'https://api.open5e.com/v1/magicitems/',
-    weapons: 'https://api.open5e.com/v2/weapons/',
-    armor: 'https://api.open5e.com/v2/armor/',
+    weapons: 'https://api.open5e.com/v1/weapons/',
+    armor: 'https://api.open5e.com/v1/armor/',
   };
 
   constructor(private http: HttpClient) {}
 
   search(category: keyof typeof this.endpoints, query: string): Observable<any> {
     const url = `${this.endpoints[category]}?search=${query}`;
-    //console.log(`${this.endpoints[category]}?search=${query}`);
     return this.http.get<any>(url);
   }
 
