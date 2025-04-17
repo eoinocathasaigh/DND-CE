ce258d6e28ef61db3c579013f4b600eb41aec3f5
Eoin Ocathasaigh <g00417466@atu.ie>
2025-04-17 16:32:37 +0100

    Removed things relating to the settings function as we wont be needing it

--- DND-CE/src/app/app.routes.ts
+++ DND-CE/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './components/home/home.component';
 import { CharacterComponent } from './components/character/character.component';
 import { CombatComponent } from './components/combat/combat.component';
 import { DiceComponent } from './components/dice/dice.component';
-import { SettingsComponent } from './components/settings/settings.component';
 import { CreateCombatComponent } from './components/create-combat/create-combat.component';
 import { PlayCombatComponent } from './components/play-combat/play-combat.component';
 import { SearchComponent } from './components/search/search.component';
@@ -17,15 +16,9 @@ export const routes: Routes = [
     { path: "combat", component: CombatComponent },
     { path: "createCombat", component: CreateCombatComponent },
     { path: "dice", component: DiceComponent },
-    { path: "settings", component: SettingsComponent },
     { path: "search", component: SearchComponent },
     { path: 'character-sheet-maker', component: CharacterSheetMakerComponent },
     { path: 'character-sheet/:id', component:  CharacterSheetComponent},
     { path: 'character-update/:id', component:  CharacterUpdateComponent},
-
-
-    {
-        path: 'play-encounter/:id',
-        component: PlayCombatComponent
-    }
+    { path: 'play-encounter/:id', component: PlayCombatComponent}
 ];
